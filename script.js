22b897e59cd522e4b0f1c06485f72354dab665a1
DannyIbz <danisanchez86@gmail.com>
2020-10-02 19:04:13 +0200

    Exercice finished.

--- script.js
+++ script.js
@@ -38,12 +38,16 @@ WARNING!!! WARNING!!!
 
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
+  var lowerNames = names.map(x => x.toLowerCase());
+
   // STEP 10:
   // Loop over the names array and say either 'Hello' or "Good Bye"
   // using the 'speak' method or either helloSpeaker's or byeSpeaker's
-  // 'speak' method.
-  // See Lecture 50, part 1
-  for (/* fill in parts of the 'for' loop to loop over names array */) {
+  // 'speak' method. 
+  // See Lecture 50, part 1 -> DONE
+  for (let name of lowerNames) {
+
+    // console.log(byeSpeaker.speak(name));
 
     // STEP 11:
     // Retrieve the first letter of the current name in the loop.
@@ -51,20 +55,20 @@ WARNING!!! WARNING!!!
     // names that start with either upper case or lower case 'J'/'j', call
     // string object's 'toLowerCase' method on the result so we can compare
     // to lower case character 'j' afterwards.
-    // Look up these methods on Mozilla Developer Network web site if needed.
-    // var firstLetter =
+    // Look up these methods on Mozilla Developer Network web site if needed. -> DONE
+
+    var firstLetter = name.charAt(0);
 
     // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
     // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-    // name in the loop.
-    if (/* fill in condition here */) {
-      // byeSpeaker.xxxx
+    // name in the loop. -> DONE
+    if (firstLetter == "j") {
+      byeSpeaker.speak(name);
     } else {
-      // helloSpeaker.xxxx
+      helloSpeaker.speak(name);
     }
   }
 
 })();
-
